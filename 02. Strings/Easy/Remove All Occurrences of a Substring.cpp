d9be978ba705a4a75d7fc611752d9034c00711aa
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-07-27 00:01:38 +0530

    feat: add Remove All Occurrences of a Substring solution

---
+++ 02. Strings/Easy/Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,29 @@
+/*
+==================================================
+Problem      : Remove All Occurrences of a Substring
+LeetCode     : 1910
+Difficulty    : Medium
+Topic         : String
+Approach      : find() + erase()
+
+Time Complexity  : O(n^2) - Worst Case
+Space Complexity : O(1) - Auxiliary Space
+==================================================
+*/
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        size_t pos; // In short: size_t pos; creates a variable to store the index returned by s.find(part). 
+                    // size_t is the standard unsigned integer type used for string/container sizes and indices in C++.
+
+        while ((pos = s.find(part)) != string::npos) {
+            s.erase(pos, part.length());
+        }
+
+        return s;
+    }
+};
