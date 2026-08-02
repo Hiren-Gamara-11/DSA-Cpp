f6ea2be63c874b855e2395ef9818a200e921d1b0
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-08-02 23:35:24 +0530

    feat: add Reverse Pairs solution

---
+++ 01. Arrays/Sorting/Merge Sort/493. Reverse Pairs/Solution.cpp
@@ -0,0 +1,23 @@
+/*
+==========================================================
+493. Reverse Pairs (TUF - Array_Hard)
+==========================================================
+
+Approach:
+- Merge Sort
+- Count Reverse Pairs During Merge
+
+Time Complexity:
+- O(n log n)
+
+Space Complexity:
+- O(n)
+
+Concepts Used:
+- Divide and Conquer
+- Merge Sort
+- Two Pointers
+- Integer Overflow Handling (2LL)
+
+==========================================================
+*/
