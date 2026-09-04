37726f101c709d2178b315e359daef2296ca870a
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-09-04 20:30:14 +0530

    add file

--- 01. Arrays/Hash Tabel/1.cpp
+++ 01. Arrays/Hash Tabel/1.cpp
@@ -6,5 +6,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "This is my first C++ program." << endl;
     cout << "I am learning C++ programming." << endl;
+    cout << "Today is Janmashtami. still i am learning." << endl;
     return 0;
 }
