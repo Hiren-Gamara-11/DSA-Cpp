aaa9dc535f7c76a6a2b2e338a4afaad9a4138c49
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-09-04 20:32:54 +0530

    add brute force approach

--- 01. Arrays/Hash Tabel/1.cpp
+++ 01. Arrays/Hash Tabel/1.cpp
@@ -6,6 +6,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "This is my first C++ program." << endl;
     cout << "I am learning C++ programming." << endl;
-    cout << "Today is Janmashtami. still i am learning." << endl;
+    cout << "Today is Janmashtami. still i am learning.(04/09/2026)" << endl;
     return 0;
 }
