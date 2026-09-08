597c56668b105e30a142036d2a0f64616fb12487
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-09-08 23:13:33 +0530

    add optimal arrpoach

--- 01. Arrays/Hash Tabel/1.cpp
+++ 01. Arrays/Hash Tabel/1.cpp
@@ -11,5 +11,6 @@ int main()
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
+    cout << "I am learning c++ programming." << endl;
     return 0;
 }
