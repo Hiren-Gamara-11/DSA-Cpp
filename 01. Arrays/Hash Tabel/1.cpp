166d134e5b4863151b55ac599159f3fc67ca2638
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-09-10 23:45:43 +0530

    try to make simple

--- 01. Arrays/Hash Tabel/1.cpp
+++ 01. Arrays/Hash Tabel/1.cpp
@@ -13,5 +13,6 @@ int main()
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning c++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
+    cout << "I amm learning C++ programming." << endl;
     return 0;
 }
