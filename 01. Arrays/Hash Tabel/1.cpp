ed79c5034a82b2116a52d2c63ddd7360d1af698f
Hiren-Gamara-11 <hirengamara544@gmail.com>
2026-09-14 21:35:03 +0530

    add brute force approach

--- 01. Arrays/Hash Tabel/1.cpp
+++ 01. Arrays/Hash Tabel/1.cpp
@@ -11,11 +11,12 @@ int main()
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
-    cout << "I am learning c++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
-    cout << "I amm learning C++ programming." << endl;
-    cout << "I amm learning C++ programming." << endl;
-    cout << "I am learning c++ programming" << endl;
+    cout << "I am learning C++ programming." << endl;
+    cout << "I am learning C++ programming." << endl;
+    cout << "I am learning C++ programming." << endl;
+    cout << "I am learning C++ programming." << endl;
+    cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
     cout << "I am learning C++ programming." << endl;
 
